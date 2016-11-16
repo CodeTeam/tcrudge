f938f45e30a8801f7bc64a4e27731c746bd98769
Алина <akkrasavina@sberned.ru>
2016-11-16 17:26:17 +0300

    readthedocs fails on gitpages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
